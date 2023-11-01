ed58bc5a0f327af9b13dddc3bb853b6125e1268f
BerkeAnna <berke.anna0803@gmail.com>
2023-11-01 09:09:54 +0100

    basic

---
+++ mestint_agents1_tavalyi_működő/README.md
@@ -0,0 +1,4 @@
+javac -cp game_engine.jar SamplePlayer.java
+
+java -jar game_engine.jar 0 game.racetrack.RaceTrackGame 11 27 5 0.1 10 1234567890 1000 SamplePlayer
+
--- mestint_agents1_tavalyi_működő/src/Main.java
+++ mestint_agents1_tavalyi_működő/src/Main.java
@@ -8,7 +8,7 @@ public class Main {
         int rand_int1 = ThreadLocalRandom.current().nextInt();
         String szam = Integer.toString(rand_int1);
 
-        String[] args1 = {"1", "game.racetrack.RaceTrackGame", "11", "27", "5", "0.1", "10",
+        String[] args1 = {"1", "game.racetrack.RaceTrackGame", "5", "10 ", "5", "0.1", "10",
                 "1234567890", "1000", "SamplePlayer"};
 
         try{
