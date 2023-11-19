72f002e1baa8c694faf78290d84125871d8aef3a
BerkeAnna <berke.anna0803@gmail.com>
2023-11-19 21:03:15 +0100

    most jó, dde 1es közre + lesz

--- racetrack2.0/SamplePlayer.java
+++ racetrack2.0/SamplePlayer.java
@@ -1,3 +1,5 @@
+///BAnna, Berke.Anna.Vivien@stud.u-szeged.hu
+
 import game.racetrack.Direction;
 import game.racetrack.RaceTrackGame;
 import game.racetrack.RaceTrackPlayer;
@@ -8,8 +10,7 @@ import java.util.Random;import java.util.*;
 public class SamplePlayer extends RaceTrackPlayer {
     private int[][] track;
     private int[] goalPosition;
-    private Set<Node> visited;
-    private static final int SPEED = 1; // Consistent speed
+    private static final int SPEED = 1;
 
     private Direction DOWN = RaceTrackGame.DIRECTIONS[7];
     private Direction LEFT = RaceTrackGame.DIRECTIONS[1];
@@ -21,7 +22,23 @@ public class SamplePlayer extends RaceTrackPlayer {
         super(state, random, track, coins, color);
         this.track = track;
         this.goalPosition = findGoalPosition();
-        this.visited = new HashSet<>();
+    }
+
+
+    /**
+     * Ellenőrzi, hogy az adott koordinátájú mező léphető-e
+     * @param i - 1. koordináta
+     * @param j - 2. koordináta
+     * @return ha a mezőn fal van, vagy a pályán kívüli mező lenne hamisat ad vissza, ha léphető igazat
+     */
+    private boolean canMoveTo(int i, int j) {
+        if (i < 0 || i >= track.length) {
+            return false;
+        }
+        if(j < 0 || j >= track[0].length){
+            return false;
+        }
+        return (track[i][j] & RaceTrackGame.WALL) == 0;
     }
 
     /**
@@ -48,33 +65,7 @@ public class SamplePlayer extends RaceTrackPlayer {
         return node.i == goalPosition[0] && node.j == goalPosition[1];
     }
 
-    /**
-     * Rekonstruálja az utat
-     * @param célmező
-     * @return ha üres az útvonal egy helyben marad, ha nem üres, az útvonal első elemét adja vissza
-     */
-    private Direction reconstructPath(Node goal) {
-        LinkedList<Direction> path = new LinkedList<>();
-        Node current = goal;
-        while (current.parent != null) {
-            path.addFirst(new Direction(current.i - current.parent.i, current.j - current.parent.j));
-            current = current.parent;
-        }
-        return path.isEmpty() ? STAY : path.getFirst();
-    }
 
-    /**
-     * Ellenőrzi, hogy az adott koordinátájú mező léphető-e
-     * @param i - 1. koordináta
-     * @param j - 2. koordináta
-     * @return ha a mezőn fal van, vagy a pályán kívüli mező lenne hamisat ad vissza, ha léphető igazat
-     */
-    private boolean canMoveTo(int i, int j) {
-        if (i < 0 || i >= track.length || j < 0 || j >= track[0].length) {
-            return false;
-        }
-        return (track[i][j] & RaceTrackGame.WALL) == 0;
-    }
 
     /**
      * Kiszámolja a megadott koordináták alapján a mező milyen távolságra van a célponttól
@@ -86,10 +77,32 @@ public class SamplePlayer extends RaceTrackPlayer {
         return Math.abs(i - goalPosition[0]) + Math.abs(j - goalPosition[1]);
     }
 
+
+    /**
+     * Rekonstruálja az utat, visszaköveti az utat kezdőponttól, célpontig
+     * @param célmező
+     * @return ha üres az útvonal egy helyben marad, ha nem üres, az útvonal első elemét adja vissza
+     */
+    private Direction reconstructRoute(Node goal) {
+        LinkedList<Direction> route = new LinkedList<>();
+        Node current = goal;
+        while (current.parent != null) {
+            route.addFirst(new Direction(current.i - current.parent.i, current.j - current.parent.j));
+            current = current.parent;
+        }
+        return route.isEmpty() ? STAY : route.getFirst();
+    }
+
+
+    /**
+     * Kiszámítja a következő lépést
+     * @param timeBudget
+     * @return A következő lépéssel tér vissza
+     */
     @Override
     public Direction getDirection(long timeBudget) {
         Node startNode = new Node(state.i, state.j, null, 0, calcHeuristic(state.i, state.j));
-        PriorityQueue<Node> openNodes = new PriorityQueue<>(Comparator.comparingInt(Node::f));
+        PriorityQueue<Node> openNodes = new PriorityQueue<>((node1, node2) -> node1.f() - node2.f());
         Set<Node> closedNodes = new HashSet<>();
         openNodes.add(startNode);
 
@@ -101,13 +114,13 @@ public class SamplePlayer extends RaceTrackPlayer {
             closedNodes.add(currentNode);
 
             if (isGoal(currentNode)) {
-                return reconstructPath(currentNode);
+                return reconstructRoute(currentNode);
             }
 
-            for (int di = -SPEED; di <= SPEED; di++) {
-                for (int dj = -SPEED; dj <= SPEED; dj++) {
-                    int nextRow = currentNode.i + di;
-                    int nextColumn = currentNode.j + dj;
+            for (int vi = SPEED; vi >= -SPEED; vi--) {
+                for (int vj = SPEED; vj >= -SPEED; vj--) {
+                    int nextRow = currentNode.i + vi;
+                    int nextColumn = currentNode.j + vj;
 
                     if (!canMoveTo(nextRow, nextColumn)) {
                         continue;
@@ -147,54 +160,30 @@ public class SamplePlayer extends RaceTrackPlayer {
             return g + h;
         }
 
+        /**
+         * Ellenőrzi, hogy a két Node objektum egyenlő-e
+         * @param object
+         * @return Összehasonlítja a két Node object i és j koordinátáit,
+         *  ellenőrzi, hogy a paraméterben kapott object nem null és azonos osztályú-e, mint a Node
+         *  az object önmagával egyezik-e
+         */
         @Override
         public boolean equals(Object object) {
-            if (this == object) return true;
-            if (object == null || getClass() != object.getClass()) return false;
+            if (this == object) { return true; }
+            if (object == null || getClass() != object.getClass()){ return false; }
             Node node = (Node) object;
             return i == node.i && j == node.j;
         }
 
+        /**
+         * Egész számot generál, ami az objektum tartalmát mutatja
+         * @return generál egy számot, ami az i és j értékeit veszi figyelembe
+         */
         @Override
         public int hashCode() {
             return Objects.hash(i, j);
         }
     }
 
-   /* private void printCostsTable() {
-        // Temporary variables to simulate the state and other necessary data
-        int currentRow = 0; // Example starting row
-        int currentColumn = 0; // Example starting column
-        int currentSpeedI = 0; // Example speed I component
-        int currentSpeedJ = 0; // Example speed J component
-
-        System.out.println("f-costs Table:");
-        for (int i = 0; i < track.length; i++) {
-            for (int j = 0; j < track[i].length; j++) {
-                Node node = new Node(i, j, null, calculateGCost(i, j, currentRow, currentColumn, currentSpeedI, currentSpeedJ), calcHeuristic(i, j));
-                System.out.print(node.f() + "\t");
-            }
-            System.out.println();
-        }
-
-        System.out.println("\ng-costs Table:");
-        for (int i = 0; i < track.length; i++) {
-            for (int j = 0; j < track[i].length; j++) {
-                Node node = new Node(i, j, null, calculateGCost(i, j, currentRow, currentColumn, currentSpeedI, currentSpeedJ), calcHeuristic(i, j));
-                System.out.print(node.g + "\t");
-            }
-            System.out.println();
-        }
-    }
-
-
-    private int calculateGCost(int i, int j, int currentRow, int currentColumn, int currentSpeedI, int currentSpeedJ) {
-        // Implement the logic to calculate the g-cost from the starting position to (i, j)
-        // This could be a simple distance calculation or more complex based on your game's rules
-        // For a basic example, you could use the Manhattan distance:
-        return Math.abs(i - currentRow) + Math.abs(j - currentColumn);
-    }
-
-*/
 
 }
