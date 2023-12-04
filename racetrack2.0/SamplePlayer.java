8689cac730f9e9adf7c4daaddf1fcdf5e6d4815a
BerkeAnna <berke.anna0803@gmail.com>
2023-12-04 08:30:44 +0100

    van ahol timeout

--- racetrack2.0/SamplePlayer.java
+++ racetrack2.0/SamplePlayer.java
@@ -1,112 +1,55 @@
 import game.racetrack.Direction;
 import game.racetrack.RaceTrackGame;
 import game.racetrack.RaceTrackPlayer;
-import game.racetrack.utils.Coin;
 import game.racetrack.utils.Cell;
+import game.racetrack.utils.Coin;
 import game.racetrack.utils.PathCell;
 import game.racetrack.utils.PlayerState;
+
 import java.util.*;
 
-/**
- * jatekos implementalasa
- * tartalmazza a jatekos logikajat, utvonal keresest
- */
 public class SamplePlayer extends RaceTrackPlayer {
     private int[][] track;
-    private int[] goalPosition;
+    private Cell goalPosition;
     private static final int SPEED = 1;
 
-
-    /**
-     * iranyok
-     */
     private Direction DOWN = RaceTrackGame.DIRECTIONS[7];
     private Direction LEFT = RaceTrackGame.DIRECTIONS[1];
     private Direction RIGHT = RaceTrackGame.DIRECTIONS[5];
     private Direction UP = RaceTrackGame.DIRECTIONS[3];
     private Direction STAY = RaceTrackGame.DIRECTIONS[0];
 
-    /**
-     * Konstruktor
-     *
-     * @param state - kezdeti jatekos allapot
-     * @param random - veletlenszam-genertor
-     * @param track - a palya
-     * @param coins - ermek tombje, amik a palyan vannak
-     * @param color - szinek
-     */
     public SamplePlayer(PlayerState state, Random random, int[][] track, Coin[] coins, int color) {
         super(state, random, track, coins, color);
         this.track = track;
         this.goalPosition = findGoalPosition();
     }
 
-
-
-    /**
-     * Ellenorzi, hogy az adott koordinataju mezo lepheto-e
-     * eloszor ellenorzi, hogy a palyan belul talalhato-e a koordinata
-     * aztan elleorzi, hogy fal van-e a mezon
-     * @param i - 1. koordinata - a mezo soranak indexe
-     * @param j - 2. koordinata -a mezo oszlopanak indexe
-     * @return ha a mezon fal van, vagy a palyan kivuli mezo lenne hamisat ad vissza, ha lepheto igazat
-     */
-
     private boolean canMoveTo(int i, int j) {
-        if (i < 0 || i >= track.length) {
-            return false;
-        }
-        if(j < 0 || j >= track[0].length){
+        if (i < 0 || i >= track.length || j < 0 || j >= track[0].length) {
             return false;
         }
         return (track[i][j] & RaceTrackGame.WALL) == 0;
     }
 
-    /**
-     * A cel poziciojanak keresese
-     * @return A cel pozicio koordinatait tarolo egesz szamokból allo tomb.
-     *  *         Ha nincs cel a palyan vagy nem talal, null értékkel tér vissza.
-     */
-
-    private int[] findGoalPosition() {
+    private Cell findGoalPosition() {
         for (int i = 0; i < track.length; i++) {
             for (int j = 0; j < track[i].length; j++) {
                 if ((track[i][j] & RaceTrackGame.FINISH) == RaceTrackGame.FINISH) {
-                    return new int[]{i, j};
+                    return new Cell(i, j);
                 }
             }
         }
         return null;
     }
 
-    /**
-     * Ellenőrzi, hogy az adott mezo helyzete megegyezik-e a cellal
-     * @param cell - a vizsgalando cella, amit meg akarunk nezni, hogy azonos-e a cellal
-     * @return logikai valtozo, hogy a cella megegyezik-e a cellal
-     */
     private boolean isGoal(Cell cell) {
-        return cell.i == goalPosition[0] && cell.j == goalPosition[1];
+        return cell.same(goalPosition);
     }
-    /**
-     * Kiszamolja a megadott koordinatak alapjan a mezo milyen tavolsagra van a celponttol
-     * @param i - 1. koordinata -aktualis mezo sora
-     * @param j - 2. koordinata - aktualis mezo oszlopa
-     * @return heurisztika erteke - abszolut ertek osszege, az i és celmezo sora, a j es celmezo oszlopa
-     */
 
     private int calcHeuristic(int i, int j) {
-        return Math.abs(i - goalPosition[0]) + Math.abs(j - goalPosition[1]);
+        return Math.abs(i - goalPosition.i) + Math.abs(j - goalPosition.j);
     }
-
-
-
-    /**
-     * Rekonstrualja az utat, visszakoveti az utat a celmezotol a kezdopontig
-     * A metódus a PathCell objektumok parent adatait használja
-     * minden lepest a route-hoz ad hozza
-     * @param celmezo -  cél pozíciója található, célmező PathCell objektum
-     * @return ha ures az utvonal egy helyben marad, ha nem ures, az utvonal elso elemet adja vissza
-     */
 
     private Direction reconstructRoute(PathCell goal) {
         LinkedList<Direction> route = new LinkedList<>();
@@ -118,24 +61,15 @@ public class SamplePlayer extends RaceTrackPlayer {
         return route.isEmpty() ? STAY : route.getFirst();
     }
 
-
-
-    /**
-     * Kiszamitja a kovetkezo lepest
-     * Az a* algoritmust haszanlja a legrovidebb ut kiszamitasara
-     * @param timeBudget
-     * @return A kovetkezo lepessel iranyaval ter vissza. Ha nincs megfelelo lepes egy helyben marad
-     */
-
     @Override
     public Direction getDirection(long timeBudget) {
         PathCell startCell = new PathCell(state.i, state.j, null);
-        Map<PathCell, Integer> g = new HashMap<>();
-        Map<PathCell, Integer> h = new HashMap<>();
-        g.put(startCell, 0);
-        h.put(startCell, calcHeuristic(startCell.i, startCell.j));
+        Map<PathCell, Integer> gValues = new HashMap<>();
+        Map<PathCell, Integer> fValues = new HashMap<>();
+        gValues.put(startCell, 0);
+        fValues.put(startCell, calcHeuristic(state.i, state.j));
 
-        PriorityQueue<PathCell> openCells = new PriorityQueue<>((cell1, cell2) -> g.get(cell1) + h.get(cell1) - g.get(cell2) - h.get(cell2));
+        PriorityQueue<PathCell> openCells = new PriorityQueue<>(Comparator.comparing(fValues::get));
         Set<PathCell> closedCells = new HashSet<>();
         openCells.add(startCell);
 
@@ -160,10 +94,14 @@ public class SamplePlayer extends RaceTrackPlayer {
                     }
 
                     PathCell neighbor = new PathCell(nextRow, nextColumn, currentCell);
-                    if (!closedCells.contains(neighbor) && !openCells.contains(neighbor)) {
-                        g.put(neighbor, g.get(currentCell) + 1);
-                        h.put(neighbor, calcHeuristic(nextRow, nextColumn));
-                        openCells.add(neighbor);
+                    int newG = gValues.get(currentCell) + 1;
+
+                    if (!closedCells.contains(neighbor) || newG < gValues.getOrDefault(neighbor, Integer.MAX_VALUE)) {
+                        gValues.put(neighbor, newG);
+                        fValues.put(neighbor, newG + calcHeuristic(nextRow, nextColumn));
+                        if (!openCells.contains(neighbor)) {
+                            openCells.add(neighbor);
+                        }
                     }
                 }
             }
